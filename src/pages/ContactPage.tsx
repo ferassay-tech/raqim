23350616f4a476dee8f4d927ad6ddbe8d6bbe94e
FERAS SAY <ferassaymah@gmail.com>
2026-07-29 09:26:53 +0300

    Update Formspree endpoint

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -49,7 +49,7 @@ export default function ContactPage() {
   setError("");
 
   try {
-    const response = await fetch("https://formspree.io/f/mlgqjkyr", {
+    const response = await fetch("https://formspree.io/f/mbdnqpwp", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
